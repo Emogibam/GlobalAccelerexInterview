298e89276d4ca786c54319cc9801237af48cdb7a
Emogibam <88818922+Emogibam@users.noreply.github.com>
2022-07-18 17:16:12 +0100

    algo update

--- Question2/Program.cs
+++ Question2/Program.cs
@@ -6,13 +6,13 @@ namespace Question2
     {
         static void Main(string[] args)
         {
-            //int[] arr = { 1, 4, 3, 2 };
-            //int N = 4;
+            int[] arr = { 1, 4, 3, 2 };
+            int N = 4;
 
-            //foreach (var item in Solution.InversePermutation(arr, N))
-            //{
-            //    Console.WriteLine(item);
-            //}
+            foreach (var item in Solution.InversePermutation(arr, N))
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
--- Question2/Solution.cs
+++ Question2/Solution.cs
@@ -13,15 +13,14 @@ namespace Question2
             
             int[] result = new int[n];
             if (arr.Length != n) return Array.Empty<int>();
-            
-            // this solution also works 
-            //for (int i = 0; i < n; i++)
-            //{
-            //    result[arr[i] - 1] = i + 1;
-            //}
-            
 
-            arr.Where(x => x != 0).ToList().ForEach(x => result[x - 1] = arr.ToList().IndexOf(x) + 1);
+            for (int i = 0; i < n; i++)
+            {
+                result[arr[i] - 1] = i + 1;
+            }
+
+            // this solution also works 
+            //arr.Where(x => x != 0).ToList().ForEach(x => result[x - 1] = arr.ToList().IndexOf(x) + 1);
 
             return result;
         }
